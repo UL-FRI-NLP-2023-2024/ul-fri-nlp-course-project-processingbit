ddae8adb9f0dcf503f2c5ec17cfec4cf96b12974
Cristian <cristianbassotto21@gmail.com>
2024-05-13 17:55:48 +0200

    update llm

--- src/llm_finetune_lora.py
+++ src/llm_finetune_lora.py
@@ -9,6 +9,8 @@ import xml.etree.ElementTree as ET
 import re
 import pandas as pd
 from datasets import load_dataset
+from sklearn.metrics import f1_score, precision_score, recall_score, accuracy_score
+from sklearn.metrics import classification_report
 
 #from IPython.display import print
 from langchain.chains.conversational_retrieval.base import ConversationalRetrievalChain
@@ -33,6 +35,9 @@ from langchain_community.docstore.document import Document
 ACCESS_TOKEN = "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"
 
 LLM_MODEL = "mistralai/Mistral-7B-Instruct-v0.2"
+CLASS = 'discussion_type'
+CLASS_IN_CODEBOOK = 'Discussion'
+
 device = f"cuda:{torch.cuda.current_device()}"
 
 bnb_config = transformers.BitsAndBytesConfig(
@@ -93,34 +98,32 @@ embedding = HuggingFaceEmbeddings(
 PROMPT_TEMPLATE = """
 You are a helpful AI assistant.
 The chat below is between children about a story.
-I need you to categorize the new sentence based on the codebook enclosed by triple *.
-Answer with only one of the classes from the codebook under Term.
+I need you to categorize the new sentence based on the codebook that follows.
 
-***
+### CODEBOOK
 {codebook}
-***
 
+Answer with only one of the classes under Term.
 You can use the context of the story enclosed by triple backquotes if it is relevant.
-If you don't know the answer, just retun nan, don't try to make up an answer.
+If you don't know the answer, just return nan, don't try to make up an answer.
 
 ```
 {context}
 ```
 
-The history of the chat is as follows enclosed by [ ].
-[{history}]
+### CHAT_HISTORY
+{chat_history}
 
-### Sentence:
-{sentence}
+### SENTENCE:
+{input}
 
-### Answer:
+### ANSWER:
 """
 
 prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
 
 # Retrive the dataset to test and train the model
-dataset = load_dataset("csv", data_files="./cleaned_data/discussion_data.csv", split="train")
-train_dataset, test_dataset = dataset.train_test_split(test_size=0.2, seed=42)
+dataset = pd.read_csv('cleaned_data/discussion_type.csv')
 
 # Retrieve story from chat history
 xml_file = './data/LadyOrThetigerIMapBook.xml'
@@ -139,12 +142,12 @@ story = re.sub(r'\n+', '\n', story)
 story = story.strip()
 story_doc = [Document(page_content=story)]
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
 story_splitted = text_splitter.split_documents(story_doc)
 vectorstore = FAISS.from_documents(story_splitted, embedding)
 retreiver = vectorstore.as_retriever(
     search_type="similarity",
-    k=3,
+    k=6,
 )
 
 STORY_RETRIEVER_TEMPLATE = """
@@ -169,46 +172,68 @@ retriever_chain = create_history_aware_retriever(
 document_chain = create_stuff_documents_chain(llm, prompt_template)
 
 # Retrieve codebook
-class_to_predict = 'Discussion'
 codebook_file = './data/codebook.xlsx'
 codebook = pd.read_excel(codebook_file)
 codebook[['Class', 'Term']] = codebook['Term'].str.split(':', expand=True)
-codebook = codebook[codebook['Class'] == class_to_predict]
+codebook = codebook[codebook['Class'] == CLASS_IN_CODEBOOK]
 codebook.drop(columns=['Class'], inplace=True)
 codebook = codebook.to_string(index=False)
 
 # Construct memory
-memory = ConversationBufferWindowMemory(
-        memory_key="chat_history", 
-        return_messages=True, 
-        output_key="answer", 
-        llm=llm,
-        k=3,
-    )
+#memory_buffer = ConversationBufferWindowMemory(
+#        memory_key="chat_history", 
+#        return_messages=True, 
+#        output_key="answer", 
+#        llm=llm,
+#        k=8,
+#    )
+#
+#def categorize(sentence, codebook, memory):
+#    history = memory.load_memory_variables({})
+#    chat_history = history['chat_history']
+#
+#    docs = retriever_chain.invoke({"input": sentence, "chat_history": chat_history})
+#
+#    response = document_chain.invoke({"input": sentence, "codebook": codebook, "context": docs, "chat_history": chat_history})
+#
+#    answer = response.split('### ANSWER:\n')[1].strip()
+#
+#    memory.save_context(inputs={'input': sentence}, outputs={'answer': answer})
+#    return answer
 
-def categorize(sentence) -> str:
-    history = memory.load_memory_variables({})
-
-    docs = retriever_chain.invoke({"input": sentence, "chat_history": history})
-
-    response = document_chain.invoke({"sentence": sentence, "codebook": codebook, "context": docs[0]["text"], "history": history})
-    answer = response["answer"].split("### Answer:")[-1].strip()
-    return answer
-
-print(categorize("I think the princess will choose the lady"))
-
-
-
-
+# Test the model
+#y = dataset[CLASS].values
+#dataset.drop(columns=[CLASS], inplace=True)
+#answers = []
+#for i in range(len(dataset)):
+#    if i >= 1 and not dataset.iloc[i][['book_id', 'bookclub', 'course']].equals(dataset.iloc[i-1][['book_id', 'bookclub', 'course']]):
+#        memory_buffer.clear()
+#
+#    sentence = dataset.iloc[i]['message']
+#    answer = categorize(sentence, codebook, memory_buffer)
+#    print(answer)
+#
+#    answers.append(answer)
+#
+#print('Accuracy:', accuracy_score(y, answers))
+#print('Precision:', precision_score(y, answers, average='weighted', zero_division=0))
+#print('Recall:', recall_score(y, answers, average='weighted', zero_division=0))
+#print('F1:', f1_score(y, answers, average='weighted', zero_division=0))
+#
+#print(classification_report(y, answers, zero_division=0))
+    
+def categorize_batch(dataset, codebook):
+    
+    
 
 ############################################################
 #                           LoRA                           #
 ############################################################
 
 #from datasets import load_dataset
-
-# Load dataset discussion_data.csv
-#dataset = load_dataset("csv", data_files="discussion_data.csv", split="train")
+#
+## Load dataset discussion_data.csv
+#dataset = load_dataset("csv", data_files="./cleaned_data/discussion_data.csv", split="train")
 #
 #from peft import LoraConfig, get_peft_model
 #from transformers import TrainingArguments
@@ -229,7 +254,7 @@ print(categorize("I think the princess will choose the lady"))
 #lora_model = get_peft_model(model, peft_config)
 #
 ## TODO: finetuning
-#output_dir = "./results"
+#output_dir = "./llm_results"
 #per_device_train_batch_size = 1
 #gradient_accumulation_steps = 1
 #optim = "paged_adamw_32bit" #specialization of the AdamW optimizer that enables efficient learning in LoRA setting.
@@ -257,7 +282,7 @@ print(categorize("I think the princess will choose the lady"))
 #    lr_scheduler_type=lr_scheduler_type,
 #    report_to="none"
 #)
-#
+
 #from trl import SFTTrainer
 #
 ## TODO: finetuning
--- src/run-slurm.sh
+++ src/run-slurm.sh
@@ -3,11 +3,11 @@
 #SBATCH --partition=gpu                     # Partition (queue) name
 #SBATCH --nodes=1                           # Number of nodes
 #SBATCH --ntasks=1                          # Number of tasks (processes)
-#SBATCH --cpus-per-task=4                   # CPU cores/threads per task
-#SBATCH --gres=gpu:1                        # Number of GPUs per node
-#SBATCH --mem=4G                            # Job memory request
+#SBATCH --cpus-per-task=12                   # CPU cores/threads per task
+#SBATCH --gpus=2                        # Number of GPUs per node
+#SBATCH --mem=64G                            # Job memory request
 #SBATCH --time=02:00:00                     # Time limit hrs:min:sec
 #SBATCH --output=logs/RAG-job_%j.log        # Standard output log
 #SBATCH --error=logs/RAG-job_%j.err         # Standard error log
 
-singularity exec --nv ./containers/container_llm.sif python3 llm_finetune_lora.py
+singularity exec --nv ./containers/container_llm.sif python3 llm.py
