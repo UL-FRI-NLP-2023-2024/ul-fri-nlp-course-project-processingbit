a533a0b0a73c9a8b5ffec75716bb83c19f5be2bf
Nikolay Petrov Kormushev <nkormushev@gmail.com>
2024-05-22 23:34:21 +0200

    update XAI prompt

--- src/8_explanations.py
+++ src/8_explanations.py
@@ -53,7 +53,7 @@ if __name__ == "__main__":
     data = load_data_predicts(path_dir, class_to_predict, model_name)
     
     # Message used to ask the llm for an explanation
-    explanation_message = "Based on the previous chat, can you explain to me why you have chosen this class in a few sentences?"
+    explanation_message = "Based on the previous chat, can you explain to me why you have chosen this class in a two sentences? Use at most one hundred words. Please reference parts of the input message when explaining your decision."
 
     # Retrieve one message from the test data for proof of concept
     test_data = data.iloc[0]
